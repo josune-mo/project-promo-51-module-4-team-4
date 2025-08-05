527d2b07780c0757f59c1f6ea6e79c96c9b8d735
josune-mo <josune.mo@gmail.com>
2025-08-05 21:13:42 +0200

    Update

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+// Conexión a la BBDD con variables de entorno
+const getConnection = async () => {
+  return await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+};
+
+module.exports = getConnection;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 // Importaciones
 const express = require("express");
-const mysql = require("mysql2/promise");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
-const PORT = 11655 || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware para parsear JSON
 app.use(express.json());
@@ -16,13 +14,3 @@ app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
 
-// Conexión a la BBDD con variables de entorno
-const getConnection = async () => {
-  return await mysql.createConnection({
-    host: server-projects-server-projects.j.aivencloud.com,
-    user: avnadmin,
-    password: AVNS_9lvqi0vXNtuReoLROLE,
-    database: projects_db,
-    port: 11655,
-  });
-};
