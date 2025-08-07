b2dd73f955473402b0d819b70ae62040d886edd2
Joana2617 <joananunes33@gmail.com>
2025-08-07 17:30:05 +0100

    endpoint id

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -1,86 +1,141 @@
-
-const getConnection = require('../db/db');
-
+const getConnection = require("../db/db");
 
 const getAllProjects = async (req, res) => {
- try{
-    const conn =await getConnection();
-    const [rows] = await conn.query ("SELECT * FROM project");
+  try {
+    const conn = await getConnection();
+    const [rows] = await conn.query("SELECT * FROM project");
     await conn.end();
     res.status(200).json(rows);
- } catch (error){
-    res.status(500).json({error: error.message, message: 'Error al obtener proyectos'})
- }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Error al obtener proyectos" });
+  }
+};
 
+const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  //**Por ID Joana */
+  try {
+    const conn = await getConnection();
 
-}
+    const [rows] = await conn.query(
+      `
+      SELECT 
+        p.id, p.name, p.slogan, p.repo, p.demo, p.technologies, 
+        p.description, p.image, 
+        a.autor, a.job, a.photo
+      FROM project p
+      JOIN autores a ON p.autor_id = a.id
+      WHERE p.id = ?
+    `,
+      [id]
+    );
 
-const getProjectById =async (req, res) => {
-//**Por ID Joana */
+    await conn.end();
 
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Project not found" });
+    }
 
-}
-
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+      message: "Error retrieving project by ID",
+    });
+  }
+};
 
 const createProject = async (req, res) => {
-try{
-    const conn = await getConnection ();
-    const {name, slogan, repo, demo, technologies, description, image, autor, job, photo} = req.body;
+  try {
+    const conn = await getConnection();
+    const {
+      name,
+      slogan,
+      repo,
+      demo,
+      technologies,
+      description,
+      image,
+      autor,
+      job,
+      photo,
+    } = req.body;
 
     //**Buscamos si la autora existe.. al final creo que era más viable hacer la tabla intermedia  :P */
-    const [rows] = await conn.query ('SELECT id FROM autores WHERE autor =? AND job = ?', [autor, job])
+    const [rows] = await conn.query(
+      "SELECT id FROM autores WHERE autor =? AND job = ?",
+      [autor, job]
+    );
 
     let autor_id;
     if (rows.length > 0) {
-        autor_id = rows [0].id
+      autor_id = rows[0].id;
     } else {
-    const [insertResults] = await conn.query ('INSERT INTO autores (autor, job, photo) VALUES (?, ?, ? )', [autor, job, photo]); 
-    autor_id = insertResults.insertId;
+      const [insertResults] = await conn.query(
+        "INSERT INTO autores (autor, job, photo) VALUES (?, ?, ? )",
+        [autor, job, photo]
+      );
+      autor_id = insertResults.insertId;
     }
-    const [result] = await conn.query ('INSERT INTO project (name, slogan, repo, demo, technologies, description, image, autor_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-        [name, slogan, repo, demo, technologies, description, image, autor_id]
+    const [result] = await conn.query(
+      "INSERT INTO project (name, slogan, repo, demo, technologies, description, image, autor_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [name, slogan, repo, demo, technologies, description, image, autor_id]
     );
     await conn.end();
-    res.status(200).json({ message: 'Proyecto insertado', id: result.insertId})
-}catch (error){
-    res.status(500).json ({error: error.message})
-}
-}
+    res
+      .status(200)
+      .json({ message: "Proyecto insertado", id: result.insertId });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const updateProject = async (req, res) => {
-//** PUT - Chiara */
-    try {
-      const conn = await getConnection();
-      const id = req.params; 
-      const {name, slogan, repo, demo, technologies, description, image, autor, job, photo} = req.body;
-  
-      // Actualizar proyecto
-      const [result] = await conn.query(`UPDATE project SET name = ?, slogan = ?, repo = ?, demo = ?, technologies = ?, description = ?, image = ?, autor_id = ? WHERE id = ?`, [name, slogan, repo, demo, technologies, description, image, autor_id, id,]);
-      await conn.end();
-  
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ message: 'Project not found' });
-      }
-  
-      res.status(200).json({ message: 'Project updated successfully' });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  //** PUT - Chiara */
+  try {
+    const conn = await getConnection();
+    const id = req.params;
+    const {
+      name,
+      slogan,
+      repo,
+      demo,
+      technologies,
+      description,
+      image,
+      autor,
+      job,
+      photo,
+    } = req.body;
+
+    // Actualizar proyecto
+    const [result] = await conn.query(
+      `UPDATE project SET name = ?, slogan = ?, repo = ?, demo = ?, technologies = ?, description = ?, image = ?, autor_id = ? WHERE id = ?`,
+      [name, slogan, repo, demo, technologies, description, image, autor_id, id]
+    );
+    await conn.end();
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Project not found" });
     }
-};  
+
+    res.status(200).json({ message: "Project updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const deleteProject = async (req, res) => {
-    //**Josune */
-
-
-
-}
-
+  //**Josune */
+};
 
 //**En esta exportación si usamos llaves porque hay varias funciones a exportar */
 module.exports = {
-getAllProjects,
-getProjectById,
-createProject,
-updateProject,
-deleteProject
+  getAllProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
 };
